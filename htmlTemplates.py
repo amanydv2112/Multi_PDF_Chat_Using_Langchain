db33b21008d4e96d2c94b8570faa0dcf85ef12b1
Aman Yadav <amanyadav@Amans-MacBook-Air-2.local>
2024-01-09 23:20:49 +0530

    Initial commit

---
+++ htmlTemplates.py
@@ -0,0 +1,44 @@
+css = '''
+<style>
+.chat-message {
+    padding: 1.5rem; border-radius: 0.5rem; margin-bottom: 1rem; display: flex
+}
+.chat-message.user {
+    background-color: #2b313e
+}
+.chat-message.bot {
+    background-color: #475063
+}
+.chat-message .avatar {
+  width: 20%;
+}
+.chat-message .avatar img {
+  max-width: 78px;
+  max-height: 78px;
+  border-radius: 50%;
+  object-fit: cover;
+}
+.chat-message .message {
+  width: 80%;
+  padding: 0 1.5rem;
+  color: #fff;
+}
+'''
+
+bot_template = '''
+<div class="chat-message bot">
+    <div class="avatar">
+        <img src="https://img.freepik.com/free-vector/ai-futuristic-technology-background_53876-100681.jpg" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
+    </div>
+    <div class="message">{{MSG}}</div>
+</div>
+'''
+
+user_template = '''
+<div class="chat-message user">
+    <div class="avatar">
+        <img src="https://res.cloudinary.com/jerrick/image/upload/v1560912913/b17bazkfidttrp47vsc3.jpg">
+    </div>    
+    <div class="message">{{MSG}}</div>
+</div>
+'''
